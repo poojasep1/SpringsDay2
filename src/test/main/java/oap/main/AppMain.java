82eed8dfb2fa9853dc886d9102f5b220b5afe38f
pooja <pooja.lebaku9@gmail.com>
2021-04-21 12:20:28 +0530

    @pointcut("within(oap.model.Circle))

--- src/test/main/java/oap/main/AppMain.java
+++ src/test/main/java/oap/main/AppMain.java
@@ -3,12 +3,16 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class AppMain {
+	
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("aspect.xml");
 		ShapeService shapeService = context.getBean("shapeService",ShapeService.class);
 		//System.out.println(shapeService.getTriangle().getName());
 		System.out.println(shapeService.getCircle().getDia());
+		shapeService.getCircle().setName("semi circle");
 
+
+	
 	}
 
 }
--- src/test/main/java/oap/model/Triangle.java
+++ src/test/main/java/oap/model/Triangle.java
@@ -3,9 +3,8 @@ package oap.model;
 public class Triangle {
 String name;
 	
-	public Triangle() {
-		System.out.println("in triangle constructor");
-	}
+	public Triangle() {System.out.println("in triangle constructor");}
+
 
 	public String getName() {
 		return name;
@@ -13,6 +12,7 @@ String name;
 
 	public void setName(String name) {
 		this.name = name;
+	}me = name;
 	}
 
 }
